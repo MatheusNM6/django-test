e7b018cba6da089c8d790c79fac84145c87b6953
Matheus Magalhães <matheus.magalha12@gmail.com>
2021-11-01 10:51:05 -0300

    Renderizando html com HttpResponse

--- teste_django/settings.py
+++ teste_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fly*@oos3vlh#1hfhy6w0u84%n-41w#@-wwx0tliugtw5#jib6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
