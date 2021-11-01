6cb1ba036148893424e23848a33ce918ed9011ee
Matheus Magalhães <matheus.magalha12@gmail.com>
2021-11-01 19:13:10 -0300

    alterando o conteudo html

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<html><body><h1>teste minha aplicação</h1></body></html>")
+    return HttpResponse("<html><body><h1>teste minha aplicação. Matheus está testando isso</h1></body></html>")
