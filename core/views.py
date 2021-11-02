b4578625d618e09ba52eeb308afdde5532a51cde
Matheus Magalhães <matheus.magalha12@gmail.com>
2021-11-02 15:06:51 -0300

    Criando listagem de produtos

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<html><body><h1>teste minha aplicação. Matheus está testando isso</h1></body></html>")
+
+
+def produtos(request):
+    return HttpResponse("<html><body><h1>produto1</h1><h2>Produto2</h2><h3>Produto3</h3></body></html>")
